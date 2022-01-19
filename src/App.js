6a1ca710745f16bd6a5c7818f3a29cd93faf7925
hkiame <henrykiame@gmail.com>
2022-01-19 18:37:18 +0300

    Adds CreditCardForm component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import CreditCardForm from "./components/CreditCardForm";
 
 function App() {
+
+  let [notification, setNotification] = useState({
+    display: false,
+    success: true,
+    msg: ''
+  });
+
+  let notificationClassName = "container mt-2";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" className="">
+      {
+        notification.display ? <div id="notification" className={notificationClassName}></div> : <div className="notification"></div>
+      }
+      <CreditCardForm />
     </div>
   );
 }
---
+++ src/components/CreditCardForm.js
@@ -0,0 +1,10 @@
+const CreditCardForm = () => {
+
+    return (
+        <div id="form-wrapper">
+            <h2 className="text-center my-3 fs-1">Add Credit Card Details</h2>
+        </div>
+    );
+};
+
+export default CreditCardForm;
